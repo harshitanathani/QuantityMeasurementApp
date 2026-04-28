6f6e33a87c9aea938b8e71d8e1eb3951597327c5
Harshita Nathani <harshitad617@gmail.com>
2026-04-29 01:11:54 +0530

    Harshita Added feature/UC11-VolumeSupport

---
+++ App/src/VolumeUnit.java
@@ -0,0 +1,20 @@
+public enum VolumeUnit implements IMeasurable {
+
+    LITRE(1.0),
+    MILLILITRE(0.001),
+    GALLON(3.78541);
+
+    private final double factor;
+
+    VolumeUnit(double factor) {
+        this.factor = factor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * factor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / factor;
+    }
+}
